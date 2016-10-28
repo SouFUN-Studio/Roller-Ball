055b8f910406ae803386d42d99e61e8e4eba6031
Roberto Torres Jiménez <iruga_cross@hotmail.com>
2016-10-28 14:22:22 -0500

    Primer commit

---
+++ Assets/Scripts/FallDetect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallDetect : MonoBehaviour {
+
+    Vector3 checkpoint;
+	// Use this for initialization
+	void Start () {
+        checkpoint = new Vector3(0.0f, 0.5f, 0.0f); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.y <= -10)
+        {
+            transform.position = checkpoint;
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 0.0f, 0.0f)*0.0f);
+            gameObject.GetComponent<Rigidbody>().Sleep();
+        }
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jump;
+    public Text countText;
+    public Text winText;
+    public bool grounded = false;   
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+
+    }
+
+    void Update()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.z);
+        }
+    }
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            Destroy(other.gameObject);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void OnCollisionExit()
+    {
+        grounded = false;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "YOU WIN!!";
+        }
+    }
+}
