97b3b98b9cfe7c5cf01f49025c49ba56ea2a97f5
Roberto Torres Jiménez <iruga_cross@hotmail.com>
2016-11-04 15:36:29 -0600

    Plataforma 3

---
+++ Assets/Scripts/Bouncy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouncy : MonoBehaviour {
+
+    bool bouncy = false;
+    float bounceAmount = 10.0f;
+    Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+        if (bouncy)
+        {
+           // player.GetComponent<Rigidbody>().velocity.y = 0.0f;
+        }
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            bouncy = true;
+        }
+    }
+}
